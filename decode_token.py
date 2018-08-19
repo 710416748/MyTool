6249345af926ecc8270ae554fa5ee724553a8ecf
YeChen <710416748@qq.com>
2018-08-19 13:06:30 +0800

    done

--- decode_token.py
+++ decode_token.py
@@ -6,6 +6,9 @@ import token
 token_base64 = bytes(0)
 token_decode = bytes(0)
 head = b''
+token_ori = b'VQEBIAEQuvWSONPwfJaMR6EfHDZKFwMGc2FrdXJhAgRTuqUx'
+token_2 = b'VQECOwEQwYsuG95Vru/mHoEKTioTAgMFbG90dXMCIGJlNWRkODJmNWM4YTY'
+
 
 def decode_token():
     if len(token_base64) :
@@ -17,6 +20,9 @@ def decode_token():
         print(hex(int(head)))
         '''
         print("token len is :" + str(len(token_decode)))
+#        for i in token_decode:
+#            print(hex(int(i)), end=' ')
+        print()
         token.Token(token_decode)
 
 
@@ -31,5 +37,7 @@ if __name__ == '__main__':
     if len(sys.argv) > 1 :
         token_base64 = bytes((sys.argv[1]), encoding="utf-8")
         print(token_base64)
+    else:
+        token_base64 = token_ori
 
     decode_token()
--- token.py
+++ token.py
@@ -12,42 +12,39 @@ class Token:
         self.tvl_rang = None
         self.tvl_cpuid = None
         self.tvl_product = None
-        self.parse_tvls(token[4: len(token) - 4])
+        self.parse_tvls(token[4: len(token)])
         self.show_token()
 
     def parse_tvls(self, tvls):
+        print("tvls len : " + str(len(tvls)))
         offset = 0
         self.tvl_rang = Token.TokenTLV(tvls[offset:offset + 2])
         offset += 2
         length = self.tvl_rang.get_len()
         self.tvl_rang.set_data(tvls[offset:offset + length])
-        print("offset = " + str(offset) + " length = " + str(length))
+        #print("offset = " + str(offset) + " length = " + str(length))
         offset += length
-
 
         self.tvl_product = Token.TokenTLV(tvls[offset:offset + 2])
         offset += 2
         length = self.tvl_product.get_len()
         self.tvl_product.set_data(tvls[offset:offset+length])
-        print("offset = " + str(offset) + " length = " + str(length))
+        #print("offset = " + str(offset) + " length = " + str(length))
         offset += length
-
 
         self.tvl_cpuid = Token.TokenTLV(tvls[offset:offset + 2])
         offset += 2
         length = self.tvl_cpuid.get_len()
+        #print("cpuid len = " + str(length) + ", offset = " + str(offset))
         self.tvl_cpuid.set_data(tvls[offset:offset+length])
-        print("offset = " + str(offset) + " length = " + str(length))
+        #print("offset = " + str(offset) + " length = " + str(length))
         offset += length
-
-
 
     def show_token(self):
         self.head.show_head()
         self.tvl_rang.show_tvl()
         self.tvl_product.show_tvl()
         self.tvl_cpuid.show_tvl()
-
 
     class TokenHead:
         def __init__(self, head):
@@ -56,23 +53,17 @@ class Token:
             self.platform_id = head[2]
             self.length = head[3]
 
-        def set_head(self, head):
-            self.head = head
-
-        def set_version(self, ver):
-            self.version = ver
-
-        def set_platform_id(self, id):
-            self.platform_id = id
-
-        def set_length(self, len):
-            self.length = len
-
         def show_head(self):
             print("head: " + hex(int(self.head)))
             print("version: " + hex(int(self.version)))
-            print("platform_id: " + hex(int(self.platform_id)))
-            print("length: " + str(int(self.length)))
+            print("platform_id: " + hex(int(self.platform_id)), end='')
+            if self.platform_id == 1:
+                print("(qcom)")
+            elif self.platform_id == 2:
+                print("(mtk)")
+            elif self.platform_id == 3:
+                print("(songguo)")
+            print("data total length: " + str(int(self.length)))
 
     class TokenTLV:
         TYPE_RANG = 1
@@ -85,30 +76,35 @@ class Token:
             self.data = []
 
         def set_data(self, data):
-            print("data len: " + str(len(data)))
+            #print("data len: " + str(len(data)))
             self.data = data
 
         def get_len(self):
             return int(self.len)
 
         def show_tvl(self):
-            print("type: " + hex(int(self.type)))
+            if self.type == self.TYPE_RANG:
+                print("type: " + hex(int(self.type)) + "(TYPE_RANG)")
+            if self.type == self.TYPE_CPUID:
+                print("type: " + hex(int(self.type)) + "(TYPE_CPUID)")
+            if self.type == self.TYPE_PRODUCE:
+                print("type: " + hex(int(self.type)) + "(TYPE_PRODUCE)")
             print("len: " + str((int(self.len))))
             if self.type == self.TYPE_RANG:
-                print("data: ", end='')
+                print("rang data is : ", end='')
                 for i in self.data:
                     print(i, end=' ')
                 print()
 
             if self.type == self.TYPE_CPUID:
                 print("cpuid: ", end='')
-                print("cpuid: len " + str(len(self.data)))
-                for i in self.data:
-                    print(hex(int(i)), end='')
-                print()
+                id = 0
+                for i in range(len(self.data)):
+                    id = self.data[i] | (id << 8)
+                print(hex(int(id)))
 
             if self.type == self.TYPE_PRODUCE:
-                print("product: len " + str(len(self.data)))
-
-
-
+                print("product: ", end='')
+                for i in self.data:
+                    print(chr(int(i)), end='')
+                print()
